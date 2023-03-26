244c4fb89e105bd78161d7a8786797042b37503a
Samuel Knight <samueltobyknight@gmail.com>
2023-03-27 00:34:00 +0100

    generateSchema

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 )
 
 type Record map[string]interface{}
@@ -17,31 +18,102 @@ type Message struct {
 	BookmarkProperties []string    `json:"bookmark_properties,omitempty"`
 }
 
+// ///////////////////////////////////////////////////////////
+// Create JSON Schema
+// ///////////////////////////////////////////////////////////
+func generateSchema(records []interface{}) map[string]interface{} {
+
+	schema := make(map[string]interface{})
+	properties := make(map[string]interface{})
+
+	if len(records) > 0 {
+		for _, record := range records {
+			record, ok := record.(map[string]interface{})
+			if !ok {
+				fmt.Fprint(os.Stderr, "Error: record is not a map\n")
+				os.Exit(1)
+			}
+			for key, value := range record {
+				if _, ok := properties[key]; !ok {
+					properties[key] = make(map[string]interface{})
+					switch v := value.(type) {
+					case bool:
+						properties[key].(map[string]interface{})["type"] = "boolean"
+					case int:
+						properties[key].(map[string]interface{})["type"] = "integer"
+					case float64:
+						properties[key].(map[string]interface{})["type"] = "number"
+					case map[string]interface{}:
+						subProps := generateSchema([]interface{}{v})
+						properties[key].(map[string]interface{})["type"] = "object"
+						properties[key].(map[string]interface{})["properties"] = subProps["properties"]
+					case nil:
+						properties[key].(map[string]interface{})["type"] = "null"
+					case string:
+						if _, err := time.Parse("2006-01-02 15:04:05.999", value.(string)); err == nil {
+							properties[key].(map[string]interface{})["type"] = "timestamp"
+							break
+						} else if _, err := time.Parse("2006-01-02", value.(string)); err == nil {
+							properties[key].(map[string]interface{})["type"] = "date"
+							break
+						} else {
+							properties[key].(map[string]interface{})["type"] = "string"
+						}
+					}
+				}
+			}
+		}
+	}
+
+	schema["properties"] = properties
+	schema["type"] = "object"
+	return schema
+}
+
 func main() {
 
-	apiResponseRecordsPath := "users"
+	/////////////////////////////////////////////////////////////
+	// EXAMPLE resposne
+	/////////////////////////////////////////////////////////////
+	responseRecordsPath := "data"
 
 	apiResponse := `
-    {
-        "users": [
-            {
-                "id": 1,
-                "name": "John",
-                "email": "john@example.com",
-				"details": {
-					"location": "London, UK"
-				} 
-            },
-            {
-                "id": 2,
-                "name": "Jane",
-                "email": "jane@example.com"
-            }
-        ]
-    }
+	{
+		"data": [{
+		  "type": "articles",
+		  "id": "1",
+		  "attributes": {
+			"title": "JSON:API paints my bikeshed!",
+			"body": "The shortest article. Ever.",
+			"created": "2015-05-22T14:56:29.000Z",
+			"updated": "2015-05-22T14:56:28.000Z"
+		  },
+		  "relationships": {
+			"author": {
+			  "data": {"id": "42", "type": "people"}
+			}
+		  },
+		  "test": true,
+		  "updatedAt": "2020-01-02 15:04:05.999",
+		  "createdAt": "2020-01-02"
+		}],
+		"included": [
+		  {
+			"type": "people",
+			"id": "42",
+			"attributes": {
+			  "name": "John",
+			  "age": 80,
+			  "gender": "male"
+			}
+		  }
+		]
+	  }
     `
 
-	// Parse API response JSON into a map
+	/////////////////////////////////////////////////////////////
+	// Parse API response JSON into a Map
+	/////////////////////////////////////////////////////////////
 	var responseMap map[string]interface{}
 	err := json.Unmarshal([]byte(apiResponse), &responseMap)
 	if err != nil {
@@ -49,64 +121,46 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Output schema message for the response
-	schema := make(map[string]interface{})
-	properties := make(map[string]interface{})
-	records, ok := responseMap[apiResponseRecordsPath].([]interface{})
+	records, ok := responseMap[responseRecordsPath].([]interface{})
 	if !ok {
 		fmt.Fprint(os.Stderr, "Error: records is not an array\n")
 		os.Exit(1)
 	}
-	if len(records) > 0 {
-		records, ok := records[0].(map[string]interface{})
-		if !ok {
-			fmt.Fprint(os.Stderr, "Error: record is not a dictionary\n")
-			os.Exit(1)
-		}
-		for key, value := range records {
-			properties[key] = make(map[string]string)
-			properties[key].(map[string]string)["type"] = "string"
-			switch value.(type) {
-			case int:
-				properties[key].(map[string]string)["type"] = "integer"
-			case float64:
-				properties[key].(map[string]string)["type"] = "number"
-			}
-		}
-	}
 
-	schema["properties"] = properties
-	schema["type"] = "object"
-	keyProperties := []string{"id"}
+	/////////////////////////////////////////////////////////////
+	// OUTPUT SCHEMA message
+	/////////////////////////////////////////////////////////////
 	schemaMessage := Message{
 		Type:               "SCHEMA",
-		Stream:             apiResponseRecordsPath,
-		Schema:             schema,
-		KeyProperties:      keyProperties,
+		Stream:             responseRecordsPath,
+		Schema:             generateSchema(records),
+		KeyProperties:      []string{"id"},
 		BookmarkProperties: []string{"updated_at"},
 	}
 	schemaJson, err := json.Marshal(schemaMessage)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating Singer schema message: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error creating SCHEMA message: %v\n", err)
 		os.Exit(1)
 	}
 	fmt.Println(string(schemaJson))
 
-	// Create a Singer message for each record in the response
+	/////////////////////////////////////////////////////////////
+	// OUTPUT RECORD messages
+	/////////////////////////////////////////////////////////////
 	for _, record := range records {
 		Record, ok := record.(map[string]interface{})
 		if !ok {
-			fmt.Fprint(os.Stderr, "Error: user is not a dictionary\n")
+			fmt.Fprint(os.Stderr, "Error: user is not a map\n")
 			os.Exit(1)
 		}
 		message := Message{
 			Type:   "RECORD",
 			Data:   Record,
-			Stream: apiResponseRecordsPath,
+			Stream: responseRecordsPath,
 		}
 		messageJson, err := json.Marshal(message)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating Singer message: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error creating RECORD message: %v\n", err)
 			os.Exit(1)
 		}
 		fmt.Println(string(messageJson))
